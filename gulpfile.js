d41a5a5a77a7e628cd2022ed694b4d70795fb023
Gal <galnik.kozmelj@gmail.com>
2019-04-09 13:26:38 +0200

    update " to '

--- gulpfile.js
+++ gulpfile.js
@@ -13,20 +13,22 @@ var SCSS_SRC = "./src/Assets/scss/**/*.scss";
 var SCSS_DEST = "./src/Assets/css";
 
 //compile SCSS
-gulp.task("compile_scss", function () {
+gulp.task('compile_scss', function () {
   gulp
     .src(SCSS_SRC)
-    .pipe(sass().on("error", sass.logError))
+    .pipe(sass().on('error', sass.logError))
     .pipe(minifyCSS())
-    .pipe(rename({ suffix: ".min" }))
+    .pipe(rename({
+      suffix: '.min'
+    }))
     .pipe(changed(SCSS_DEST))
     .pipe(gulp.dest(SCSS_DEST));
 });
 
 //detect changes in SCSS
-gulp.task("watch_scss", function () {
-  gulp.watch(SCSS_SRC, ["compile_scss"]);
+gulp.task('watch_scss', function () {
+  gulp.watch(SCSS_SRC, ['compile_scss']);
 });
 
 //run tasks
-gulp.task("default", ['watch_scss']);
+gulp.task('default', ['watch_scss']); 
